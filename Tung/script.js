db67e939ff587e69a10c5b97262285b3e4bb8833
omodtung <dothedung131@gmail.com>
2023-12-04 20:37:35 +0700

    Lien He connect BackEnd

--- Tung/script.js
+++ Tung/script.js
@@ -101,32 +101,7 @@ function validateForm() {
       document.getElementById('status').innerHTML = "Message cannot be empty";
       return false;
   }
-//get input field values data to be sent to server
-//     document.getElementById('status').innerHTML = "Sending...";
-//     formData = {
-//         'name'     : $('input[name=name]').val(),
-//         'email'    : $('input[name=email]').val(),
-//         'subject'  : $('input[name=subject]').val(),
-//         'message'  : $('textarea[name=message]').val()
-//     };
 
-
-//    $.ajax({
-//     url : "mail.php",
-//     type: "POST",
-//     data : formData,
-//     success: function(data, textStatus, jqXHR)
-//     {
-
-//         $('#status').text(data.message);
-//         if (data.code) //If mail was sent successfully, reset the form.
-//         $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-//     },
-//     error: function (jqXHR, textStatus, errorThrown)
-//     {
-//         $('#status').text(jqXHR);
-//     }
-// });
 
 document.getElementById('status').innerHTML = "Sending...";
   document.getElementById('contact-form').submit();
